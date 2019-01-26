916ea419f6a81fd50671b8d24b761ca6730d7943
KononenkoAsya <ananaskononenko@gmail.com>
2019-01-26 14:31:20 +0200

    Fill the Square component with an “X” on click

--- src/components/square.js
+++ src/components/square.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {this.props.value}
-      </button>
-    );
-  }
+  	constructor(props) {
+    	super(props);
+    	this.state = {
+      		value: null,
+    	};
+  	}
+
+  	render() {
+    	return (
+      		<button
+        		className="square"
+        		onClick={ () => this.setState({value: 'X'}) }
+      		>
+        		{this.state.value}
+      		</button>
+    	);
+  	}
 }
 
 export default Square;
